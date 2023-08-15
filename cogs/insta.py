978a308a44b686280667d2f76bc24dbe40d9cc2d
dino-inc <ethan8960@gmail.com>
2023-08-14 19:09:05 -0700

    reenabling insta

--- cogs/insta.py
+++ cogs/insta.py
@@ -24,7 +24,7 @@ class Insta(commands.Cog):
         self.bot = bot
         self.insta = instaloader.Instaloader(download_video_thumbnails=False)
         insta_creds = json.load(open("./auth.json"))
-        # self.insta.login(insta_creds["username"], insta_creds["password"])
+        self.insta.login(insta_creds["username"], insta_creds["password"])
     @commands.Cog.listener()
     async def on_message(self, message):
         print("listening")
